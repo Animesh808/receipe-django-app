36c2ab3734ce0f9110623afecfc696b5ffc0fa3d
Animesh808 <animeshc567@gmail.com>
2023-03-29 12:01:14 +0600

    test case practice

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+  
+	def test_add(self):
+		res = calc.add(3,9)
+		self.assertEqual(res, 12)
+  
+	def test_subtract_numbers(self):
+		res = calc.subtract(10, 15)
+		self.assertEqual(res, 5)
